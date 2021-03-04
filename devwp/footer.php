3ef6ff563da39972418d75dcf91b178cdb77a709
Myzwer <josh@onefortyfivedesign.com>
2021-03-04 12:22:22 -0500

    :art: Fixed the style of the footer. No content changes were made.

--- devwp/footer.php
+++ devwp/footer.php
@@ -18,33 +18,29 @@
 
 
 <!-- FOOTER START -->
-<div class="red-background">
+<div class="gray-background">
     <div class="center padding-top social-links">
         <ul class="social-icons icon-circle list-inline">
-            <li> <a href="https://www.facebook.com/foothillschurchTN"><i class="fa fa-facebook"></i></a></li>
-            <li> <a href="http://instagram.com/foothillschurchtn/"><i class="fa fa-instagram"></i></a></li>
-            <li> <a href="https://twitter.com/foothillschurch"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.facebook.com/foothillschurchTN"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="http://instagram.com/foothillschurchtn/"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/foothillschurch"><i class="fa fa-twitter"></i></a></li>
         </ul>
     </div>
 
     <div class="footer-links center">
         <div class="center">
-            <?php wp_nav_menu( array( 'theme_location' => 'footer-column-1' ) ); ?>
+            <?php wp_nav_menu(array('theme_location' => 'footer-column-1')); ?>
         </div>
     </div>
 
     <hr>
 
-    <p class = "center dark-color-invert footer-text uppercase">Foothills Church exists to develop mature disciples of Christ in relational environments</p>
+    <p class="center text-color footer-text uppercase">Foothills Church exists to develop mature disciples of
+        Christ in relational environments</p>
 </div>
-</div>
+
 <!-- END OF FOOTER -->
-
-
-
 
 
 <?php wp_footer(); ?>
 
-</body>
-</html>
