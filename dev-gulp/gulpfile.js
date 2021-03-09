185783e9def757333c9a486d55adab3d630cdb8f
Myzwer <josh@onefortyfivedesign.com>
2021-03-09 12:27:04 -0500

    :green_heart: Updated gulpfile.js to use SSL.

--- dev-gulp/gulpfile.js
+++ dev-gulp/gulpfile.js
@@ -93,7 +93,7 @@ function javascript() {
 function watch() {
     browserSync.init({
         open:'external',
-        proxy: 'easters-2021.local', //switch this to whatever local generates
+        proxy: 'https://easters-2021.local', //switch this to whatever local generates
         port: 3000
     });
 
